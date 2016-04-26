f9a02a8dd6850fed14d974db201ce80c60e52091
juniorcastillo <juniormiguelcastillosantana@gmail.com>
2016-04-26 09:38:10 +0200

    Actualizado los ejercicios de Array_list

--- Array_list/Carta/CartaOrdenada.java
+++ Array_list/Carta/CartaOrdenada.java
@@ -1,9 +1,8 @@
-/*
+/**
  *Modifica el programa anterior de tal forma que las cartas se muestren ordenadas. Primero
 se ordenarán por palo: bastos, copas, espadas, oros. Cuando coincida el palo, se ordenará
 por número: as, 2, 3, 4, 5, 6, 7, sota, caballo, rey.
- */
-package Carta;
+*/
 
 /**
  *
@@ -28,15 +27,15 @@ public class CartaOrdenada {
         a.add( carta = new Carta());    
            
        }
-        while (Esta) {// combruebo si esta 
+        while (Esta) {
             for (int i = 0; i < a.size(); i++) {// recorro el arraylist
-                Carta p = (Carta) a.get(i);// guardo el valor en el objeto p
+                Carta p = (Carta) a.get(i);// guardo el valor en el object p
                 for (int q = i + 1; q < a.size(); q++) {// recorro los object creados  para ver si se repite
-                    Carta p1 = (Carta) a.get(q);//guardo el valor en obejto p1 para compararlo con p
+                    Carta p1 = (Carta) a.get(q);//guardo el valor en object p1 para compararlo con p
                     if (p.getNumero().equals(p1.getNumero()) && p.getPalo().equals(p1.getPalo())) {//condicion que se cumple
                         a.remove(q);//elimino el que se repite
                         a.add(q, carta = new Carta());// creo otronuevo donde se elimino el otro obeject
-                    } else {// cuando no se repita mas objetos  se sale de while
+                    } else {// cuando no se repita mas object se sale de while
                         Esta = false;
                     }
                 }
--- Array_list/Carta/NoRepiteCarta.java
+++ Array_list/Carta/NoRepiteCarta.java
@@ -14,7 +14,7 @@ public class NoRepiteCarta {
     public static void main(String[] args) {
         ArrayList<Carta> a = new ArrayList<Carta>();
         boolean Esta = true;
-        System.out.println("paso");
+      
 
         Carta carta = new Carta();
         a.add(carta);
@@ -27,12 +27,12 @@ public class NoRepiteCarta {
         a.add( carta = new Carta());    
            
        }
-        while (Esta) {// combruebo si esta 
+        while (Esta) {
             for (int i = 0; i < a.size(); i++) {// recorro el arraylist
                 Carta p = (Carta) a.get(i);// guardo el valor en el object p
                 for (int q = i + 1; q < a.size(); q++) {// recorro los object creados  para ver si se repite
                     Carta p1 = (Carta) a.get(q);//guardo el valor en object p1 para compararlo con p
-                    if (p.getNumero().equals(p1.getNumero()) && p.getPalo().equals(p1.getPalo())) {//condicion que se cumple
+                    if (p.getNumero().equals(p1.getNumero()) && p.getPalo().equals(p1.getPalo())) {//condiccion que se cumple
                         a.remove(q);//elimino el que se repite
                         a.add(q, carta = new Carta());// creo otronuevo donde se elimino el otro object
                     } else {// cuando no se repita mas object  se sale de while
@@ -41,7 +41,7 @@ public class NoRepiteCarta {
                 }
             }
         }
-        System.out.println("paso3");
+       
         for (Carta n : a) {
             System.out.println(n);
         }
