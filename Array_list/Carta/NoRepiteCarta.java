94389ea62289c82f103243babb751010b7a506c7
juniorcastillo <juniormiguelcastillosantana@gmail.com>
2016-04-26 09:34:27 +0200

    Actualizado los ejercicios de Array_list

--- Array_list/Carta/NoRepiteCarta.java
+++ Array_list/Carta/NoRepiteCarta.java
@@ -31,11 +31,11 @@ public class NoRepiteCarta {
             for (int i = 0; i < a.size(); i++) {// recorro el arraylist
                 Carta p = (Carta) a.get(i);// guardo el valor en el object p
                 for (int q = i + 1; q < a.size(); q++) {// recorro los object creados  para ver si se repite
-                    Carta p1 = (Carta) a.get(q);//guardo el valor en obeject p1 para compararlo con p
+                    Carta p1 = (Carta) a.get(q);//guardo el valor en object p1 para compararlo con p
                     if (p.getNumero().equals(p1.getNumero()) && p.getPalo().equals(p1.getPalo())) {//condicion que se cumple
                         a.remove(q);//elimino el que se repite
-                        a.add(q, carta = new Carta());// creo otronuevo donde se elimino el otro obeject
-                    } else {// cuando no se repita mas objetos  se sale de while
+                        a.add(q, carta = new Carta());// creo otronuevo donde se elimino el otro object
+                    } else {// cuando no se repita mas object  se sale de while
                         Esta = false;
                     }
                 }
